f13f32dd1cf4d6fd5e985d5b6b805cb8d6e00b15
pradeep-kag <pradeepkag6@gmail.com>
2025-07-17 16:42:06 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
