24bceeca82f7365e9f425ed761c4d074f2665b74
pradeep-kag <pradeepkag6@gmail.com>
2025-07-17 16:45:35 +0530

    Create SquareCalculator7.java

---
+++ SquareCalculator7.java
@@ -0,0 +1,9 @@
+public class SquareCalculator {
+    static int square(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Square of 5 is: " + square(5));
+    }
+}
