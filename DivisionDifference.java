336dcc2a00dab7596f38673a535ed5df91877ae3
pradeep-kag <pradeepkag6@gmail.com>
2025-07-17 16:43:26 +0530

    Create DivisionDifference.java

---
+++ DivisionDifference.java
@@ -0,0 +1,9 @@
+public class DivisionDifference {
+    public static void main(String[] args) {
+        int a = 7, b = 2;
+        double x = 7, y = 2;
+
+        System.out.println("Integer division: " + (a / b)); // 3
+        System.out.println("Double division: " + (x / y));  // 3.5
+    }
+}
