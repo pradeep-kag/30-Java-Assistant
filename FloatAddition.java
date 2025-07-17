0ed9612ce30b653a088be425a712c5776804fec7
pradeep-kag <pradeepkag6@gmail.com>
2025-07-17 16:43:03 +0530

    Create FloatAddition.java

---
+++ FloatAddition.java
@@ -0,0 +1,8 @@
+public class FloatAddition {
+    public static void main(String[] args) {
+        float a = 5.6f;
+        float b = 3.4f;
+        float sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
