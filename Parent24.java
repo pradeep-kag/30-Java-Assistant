d53af07fc8e116a42c39a03858da1b18f5a86e4a
pradeep-kag <pradeepkag6@gmail.com>
2025-07-17 16:54:17 +0530

    Create Parent24.java

---
+++ Parent24.java
@@ -0,0 +1,16 @@
+class Parent {
+    Parent() {
+        System.out.println("Parent Constructor");
+    }
+}
+
+class Child extends Parent {
+    Child() {
+        super();
+        System.out.println("Child Constructor");
+    }
+
+    public static void main(String[] args) {
+        new Child();
+    }
+}
